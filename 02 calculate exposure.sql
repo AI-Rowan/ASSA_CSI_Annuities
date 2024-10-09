90b965df10a078b82cdde4d1cfa23a103fcf7812
MBRGA <mburgess@rgare.com>
2024-10-09 14:18:56 +0200

    Grouped partial/no accelerator

--- 02 calculate exposure.sql
+++ 02 calculate exposure.sql
@@ -205,7 +205,8 @@ SELECT CASE
       ,life_number
       ,CASE sex_code WHEN 1 THEN 'M' WHEN 2 THEN 'F' ELSE 'U' END                                                                                               AS sex
       ,CASE smoking_category WHEN 1 THEN 'S' WHEN 2 THEN 'NS' ELSE 'U' END                                                                                      AS smoking_status
-      ,CASE accelerator_marker WHEN 1 THEN 'Fully accelerated' WHEN 2 THEN 'Partially accelerated' WHEN 3 THEN 'No accelerator' ELSE 'Unspecified' END          AS accelerator_status
+      ,CASE accelerator_marker WHEN 1 THEN 'Fully accelerated' WHEN 2 THEN 'Partially accelerated' WHEN 3 THEN 'No accelerator' ELSE 'Unspecified' END          AS accelerator_status_detailed
+      ,CASE accelerator_marker WHEN 1 THEN 'Fully accelerated' WHEN 2 THEN 'No full accelerator' WHEN 3 THEN 'No full accelerator' ELSE 'Unspecified' END       AS accelerator_status
       ,CASE COALESCE (underwriter_loadings, -99)
            WHEN 1 THEN 'EM loading 1% - 50%'
            WHEN 2 THEN 'EM loading 51% - 100%'
@@ -394,7 +395,8 @@ SELECT CASE
       ,CAST(NULL AS VARCHAR)                                                                                                                              AS life_number
       ,CASE sex_code WHEN 1 THEN 'M' WHEN 2 THEN 'F' ELSE 'U' END                                                                                         AS sex
       ,CASE smoking_category WHEN 1 THEN 'S' WHEN 2 THEN 'NS' ELSE 'U' END                                                                                AS smoking_status
-      ,CASE accelerator_marker WHEN 1 THEN 'Fully accelerated' WHEN 2 THEN 'Partially accelerated' WHEN 3 THEN 'No accelerator' ELSE 'Unspecified' END    AS accelerator_status
+      ,CASE accelerator_marker WHEN 1 THEN 'Fully accelerated' WHEN 2 THEN 'Partially accelerated' WHEN 3 THEN 'No accelerator' ELSE 'Unspecified' END    AS accelerator_status_detailed
+      ,CASE accelerator_marker WHEN 1 THEN 'Fully accelerated' WHEN 2 THEN 'No full accelerator' WHEN 3 THEN 'No full accelerator' ELSE 'Unspecified' END AS accelerator_status
       ,CASE COALESCE(underwriter_loadings,-99)
            WHEN 1 THEN 'EM loading 1% - 50%'
            WHEN 2 THEN 'EM loading 51% - 100%'
